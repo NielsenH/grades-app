64c67aa7c4c051614450c880746924e3902311e9
Nielsen Hofmam <nielsen.hofmam@gmail.com>
2020-07-07 23:13:14 -0300

    Ajustado link baseurl

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://gentle-cove-53100.herokuapp.com/',
+  baseURL: 'https://gentle-cove-53100.herokuapp.com/' ,
   headers: {
     'Content-type': 'application/json',
   },
